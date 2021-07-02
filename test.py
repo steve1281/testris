4f4aa059e3a1ad3513830a4a585b4d7c8d2ee561
steve <steve1281@hotmail.com>
2021-07-02 08:23:46 -0400

    fixed collision

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import numpy as np
 import pygame
 from pygame.locals import Rect
 
-from rgb import DARK_BLUE, RED, DARK_SLATE_GRAY, ROYAL_BLUE, ORANGE, CYAN, YELLOW, MAGENTA, GREEN, SALMON, BLACK
+from rgb import RED, DARK_SLATE_GRAY, ROYAL_BLUE, ORANGE, CYAN, YELLOW, MAGENTA, GREEN, BLACK
 from tetro import Tetro
 
 # board = np.zeros((25, 45), dtype=[ ('r', 'i4'), ('g', 'i4'), ('b', 'i4') ])
@@ -13,7 +13,7 @@ def _to_rgb(h):
     red = (h >> 16) & 0xff
     green = (h >> 8) & 0xff
     blue = h & 0xff
-    return (red, green, blue)
+    return red, green, blue
 
 
 def _to_number(r, g, b):
@@ -23,28 +23,27 @@ def _to_number(r, g, b):
     return h
 
 
-board[ 10 ][ 10 ] = _to_number(*RED)
+board[10][10] = _to_number(*RED)
 
 # create tetrominoes used in tetris
 shapes = [
-    Tetro([ (1, 0), (1, 1), (1, 2), (0, 2) ], (3, 3), _to_number(*ROYAL_BLUE)),  # J
-    Tetro([ (1, 0), (1, 1), (1, 2), (2, 2) ], (3, 3), _to_number(*ORANGE)),  # L
-    Tetro([ (0, 2), (1, 2), (2, 2), (3, 2), (4, 2) ], (5,5),  _to_number(*CYAN)),  # I
-    Tetro([ (0, 0), (0, 1), (1, 0), (1, 1) ], (2, 2), _to_number(*YELLOW)),  # O
-    Tetro([ (0, 0), (0, 1), (0, 2), (1, 1) ], (3, 3), _to_number(*MAGENTA)),  # T
-    Tetro([ (1, 0), (2, 0), (0, 1), (1, 1) ], (3, 2), _to_number(*RED)),  # Z
-    Tetro([ (0, 0), (1, 0), (1, 1), (2, 1) ], (3, 2), _to_number(*GREEN))  # S
+    Tetro([(1, 0), (1, 1), (1, 2), (0, 2)], (3, 3), _to_number(*ROYAL_BLUE)),  # J
+    Tetro([(1, 0), (1, 1), (1, 2), (2, 2)], (3, 3), _to_number(*ORANGE)),  # L
+    Tetro([(0, 2), (1, 2), (2, 2), (3, 2), (4, 2)], (5, 5), _to_number(*CYAN)),  # I
+    Tetro([(0, 0), (0, 1), (1, 0), (1, 1)], (2, 2), _to_number(*YELLOW)),  # O
+    Tetro([(0, 0), (0, 1), (0, 2), (1, 1)], (3, 3), _to_number(*MAGENTA)),  # T
+    Tetro([(1, 0), (2, 0), (0, 1), (1, 1)], (3, 2), _to_number(*RED)),  # Z
+    Tetro([(0, 0), (1, 0), (1, 1), (2, 1)], (3, 2), _to_number(*GREEN))  # S
 ]
 
 
 def _shape(tetro: Tetro):
     """ return matrix representing """
-    temp = np.zeros((tetro.shape[ 0 ], tetro.shape[ 1 ])) # , dtype=[ ('r', 'i4'), ('g', 'i4'), ('b', 'i4') ])
+    temp = np.zeros((tetro.shape[0], tetro.shape[1]))  # , dtype=[ ('r', 'i4'), ('g', 'i4'), ('b', 'i4') ])
     temp.fill(BOARD_BACKGROUND_COLOR)
     for i in tetro.tetro:
-        temp[ i[ 0 ] ][ i[ 1 ] ] = tetro.color
+        temp[i[0]][i[1]] = tetro.color
     return temp
-
 
 
 # init pygame
@@ -73,11 +72,20 @@ clock = pygame.time.Clock()
 shape_cnt = 0
 m_x = 0
 m_y = 40 // 2
-m = _shape(shapes[ 0 ])
+m = _shape(shapes[0])
+
+
+def is_collision(n_x, n_y):
+    check_board = board[n_x:n_x + m.shape[0], n_y:n_y + m.shape[1]]
+    return np.size(np.nonzero(np.logical_and(m, check_board)))
+
 
 # -------- Main Program Loop -----------
 while carryOn:
     # --- Main event loop
+    board.fill(BOARD_BACKGROUND_COLOR)
+    board[10, 10] = _to_number(*RED)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             carryOn = False
@@ -85,34 +93,29 @@ while carryOn:
             if event.key == pygame.K_ESCAPE:
                 carryOn = False
             if event.key == pygame.K_LEFT:
-                m_y = m_y - 1
+                if not is_collision(m_x, m_y - 1):
+                    m_y = m_y - 1
             if event.key == pygame.K_UP:
                 m = np.rot90(m)
                 m = np.rot90(m)
                 m = np.rot90(m)
+                if is_collision(m_x, m_y):
+                    m = np.rot90(m)
             if event.key == pygame.K_DOWN:
-                m_x = m_x + 1
-                board.fill(BOARD_BACKGROUND_COLOR)
-                board[ 10, 10 ] = _to_number(*RED)
                 # notes: want to place in m_x:m_x + m.shape[0], m_y:m_y+m.shape[1]
                 # so question is, is there anything there already?
                 # this can be checked with nonzero
-                check_board = board[m_x:m_x + m.shape[0],m_y:m_y + m.shape[1]]
-                print(m)
-                print(check_board)
-                ccm = np.nonzero(np.logical_and(m, check_board))
-                print(ccm)
-                if ccm[0].any():  # check x axis for collision possibilities
-                    m_x = m_x - 1  # for now, assume collision
-
+                if not is_collision(m_x + 1, m_y):
+                    m_x = m_x + 1
 
             if event.key == pygame.K_RIGHT:
-                m_y = m_y + 1
+                if not is_collision(m_x, m_y + 1):
+                    m_y = m_y + 1
             if event.key == pygame.K_SPACE:
                 shape_cnt = shape_cnt + 1
                 if shape_cnt == 7:
                     shape_cnt = 0
-                m = _shape(shapes[ shape_cnt ])
+                m = _shape(shapes[shape_cnt])
 
     screen.fill(DARK_SLATE_GRAY)
     board.fill(BOARD_BACKGROUND_COLOR)
@@ -121,13 +124,13 @@ while carryOn:
     ICON_X = 800 / 40
     ICON_Y = 400 / 20
 
-    board[ m_x:m_x + m.shape[ 0 ], m_y:m_y + m.shape[ 1 ] ] = m
-    board[ 10, 10 ] = _to_number(*RED)
+    board[m_x:m_x + m.shape[0], m_y:m_y + m.shape[1]] = m
+    board[10, 10] = _to_number(*RED)
 
-    pygame.draw.rect(screen, ROYAL_BLUE, [ OFFSET_X, OFFSET_Y, PLOT_X, PLOT_Y ], 0)
+    pygame.draw.rect(screen, ROYAL_BLUE, [OFFSET_X, OFFSET_Y, PLOT_X, PLOT_Y], 0)
     # note that we render 5 less, to allow for sprite overflow.
-    for y, row in enumerate(board[ :-5 ]):
-        for x, col in enumerate(row[ :-5 ]):
+    for y, row in enumerate(board[:-5]):
+        for x, col in enumerate(row[:-5]):
             pygame.draw.rect(screen, _to_rgb(col),
                              Rect(OFFSET_X + x * ICON_X, OFFSET_Y + y * ICON_Y, ICON_X, ICON_Y), 0)
 
